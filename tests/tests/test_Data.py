42037927e77ea4c934bcadd1c7667ee202d4073e
swalker92 <zwap082@live.rhul.ac.uk>
2019-07-12 14:58:47 +0100

    tests for IndexFromNearestS in light of revert for it to WORK!!

--- tests/tests/test_Data.py
+++ tests/tests/test_Data.py
@@ -8,10 +8,16 @@ PATH_TO_TEST_INPUT = "{}/../test_input/".format(
     os.path.dirname(os.path.abspath(__file__)))
 
 @pytest.fixture
-def atf2():
+def atf2_tfs_path():
     return "{}/atf2-nominal-twiss-v5.2.tfs.tar.gz".format(
         PATH_TO_TEST_INPUT)
 
-@pytest.mark.sanity
-def test_loading_atf2(atf2):
-    pymadx.Data.Tfs(atf2)
+@pytest.fixture()
+def atf2(atf2_tfs_path):
+    return pymadx.Data.Tfs(atf2_tfs_path)
+
+def test_IndexFromNearestS(atf2):
+    assert atf2.IndexFromNearestS(0.) == 3
+    assert atf2.IndexFromNearestS(0.2) == 3
+    assert atf2.IndexFromNearestS(0.5) == 9
+    assert atf2.IndexFromNearestS(0.6) == 12
