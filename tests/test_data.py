de9afcfc7dd015663068362a3678343759d82fe3
Laurie Nevay <laurie.nevay@cern.ch>
2025-01-09 23:10:14 +0100

    Start to redo testing.

---
+++ tests/test_data.py
@@ -0,0 +1,15 @@
+import os
+import pymadx
+
+
+def _fn(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
+
+
+def test_data_tfs_load():
+    d = pymadx.Data.Tfs(_fn("test_input/h6-simple.tfs"))
+    assert len(d) == 226
+
+
+def test_data_tfs_load_gz():
+    d = pymadx.Data.Tfs(_fn("test_input/h6-positive-120gev-fm.tfs.gz"))
