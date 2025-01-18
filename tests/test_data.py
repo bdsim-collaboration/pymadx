8c942d38dc65496e3975be92cb0b9f0c0b5b58df
Laurie Nevay <laurie.nevay@cern.ch>
2025-01-18 14:06:08 +0100

    More machines for testing.

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,14 +3,19 @@ import pymadx
 
 
 def _fn(filename):
-    return os.path.join(os.path.dirname(__file__), filename)
+    return os.path.join(os.path.dirname(__file__), "test_input", filename)
 
 
 def test_data_tfs_load():
-    d = pymadx.Data.Tfs(_fn("test_input/h6-simple.tfs"))
+    d = pymadx.Data.Tfs(_fn("h6-simple.tfs"))
+    assert len(d) == 226
+
+
+def test_data_tfs_survey_load():
+    d = pymadx.Data.Tfs(_fn("h6-survey.tfs"))
     assert len(d) == 226
 
 
 def test_data_tfs_load_gz():
-    d = pymadx.Data.Tfs(_fn("test_input/h6-positive-120gev-fm.tfs.gz"))
+    d = pymadx.Data.Tfs(_fn("h6-positive-120gev-fm.tfs.gz"))
     assert len(d) == 226
